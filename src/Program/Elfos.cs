db51e6fb01c3dfe2f77273f746c9dbc56986c553
OliverBillig <oliver.billig2007@gmail.com>
2026-09-07 10:50:03 -0300

    ultimo elfo clase 7/9

--- src/Program/Elfos.cs
+++ src/Program/Elfos.cs
@@ -1,10 +1,24 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Dynamic;
 
 namespace Ucu.Poo.RolePlayGame
 {
     public class Elfos
     {
-        
-        
+        public string Nombre { get ;  set; }
+        public int Vida { get ;  set; }
+
+        public Elfos(string nombre, int vida)
+        {
+            this.Nombre = nombre;
+            this.Vida = vida;
+        }
+
+        public int AtaqueElfico()
+        {
+            
+        }
     }
 }
