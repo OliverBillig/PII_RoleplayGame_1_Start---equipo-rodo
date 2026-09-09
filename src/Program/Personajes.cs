7b4e1a534cee68dc67bc2c5a4c0b330f018305d2
OliverBillig <oliver.billig2007@gmail.com>
2026-09-09 10:02:27 -0300

    personajes

--- src/Program/Personajes.cs
+++ src/Program/Personajes.cs
@@ -1,9 +1,10 @@
 using System;
 using Ucu.Poo.RolePlayGame;
-
 public class Personajes
 {
 	private Enanos enano1 = new Enanos("Jose", 100);
-
+	private Elfos elfo1 = new Elfos("Emi", 80);
+	private Caballero caballero1 = new Caballero("Joaquin", 90);
+	private Mago mago1 = new Mago("Voldemort", 70);
 	
 }
--- src/Program/Wizard.cs
+++ src/Program/Wizard.cs
@@ -2,9 +2,20 @@ using System;
 
 namespace Ucu.Poo.RolePlayGame
 {
-    
+    public class Mago
+    {
+        public string Nombre { get ;  set; }
+        public int Vida { get ;  set; }
 
+        public Mago(string nombre, int vida)
+        {
+            this.Nombre = nombre;
+            this.Vida = vida;
+        }
 
-
-    
+        public int AtaqueMagico()
+        {
+            
+        }
+    }
 }
